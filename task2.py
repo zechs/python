b6dc4d7823b1053c7475fa9d9bceaf5477e603ce
zechs <noname.otaku@gmail.com>
2013-02-28 21:18:58 +0400

    modified:   task2.py

--- task2.py
+++ task2.py
@@ -7,17 +7,19 @@ class Menu:
     def __init__(self, model, items):
         self.model = model
         self.items = items
-        
+                                
     def show(self):
         "Вывести позиции в пронумерованном виде"
         for index,item in enumerate(self.items , 1):
-               print( "[{index}]: {itemtext}".format(
+            print( "[{index}]: {itemtext}".format(
                    index = index,
                    itemtext = item.text ) )
     def ask(self):
         """ Запросить номер выбранной команды у пользователя
             и вернуть выбраную команду"""
-        index = int(input("Введите команду: ")) - 1
+        index = -1  # задание 0
+        while index >(len(self.items) - 1) or index < 0:
+            index = int(input("Введите команду: ")) - 1
         return self.items[index]
     
     def run(self):
@@ -37,39 +39,65 @@ class User:
     def __init__(self,name):
         self.name = name
         self.notes = []
-
+                        
 class Model:
     "Общая модель приложения"
+
+    users = {}  # задание 3 
+
+    def menuName(model):
+        if model.currentMenu == model.userMenu:
+            print ("Список комманд пользователя %s" \
+                   % (model.currentUser.name))
+            
     def __init__(model):
         model.currentMenu = model.logInScreen
         model.currentUser = None
-
+        
     def run(model):
         while model.currentMenu is not None:
+            print ("Menu")
             model.currentMenu.run()
 
     noteSep = "-" * 20
-    
+
     "Список команд"
     def logIn(model):
         userName = input("Введите имя пользователя: ")
         model.currentUser = User( name = userName )
         model.currentMenu = model.userMenu
-
+        
+        for key in model.users :    # задание 3
+            if key == userName :
+                model.currentUser.notes = model.users[key]                
+        
+        
     def addNote(model):
         model.currentUser.notes.append(
             input("Введите заметку: ") )
-
+                        
+    def delNote(model):     # задание 1
+        i = int(input("Введите заметку на удаление: ")) - 1
+        del model.currentUser.notes[i]
+        
     def listNotes(model):
         print(model.noteSep)
-        for note in model.currentUser.notes:
-            print(note)
+        for note in model.currentUser.notes :   # задание 2
+            l = len(note)
+            s = 15  # длина строки
+            if l > s :
+                for i in range (int(l/s) + 1):
+                    print(note[i * s : (i + 1) * s])
+                print("")    # "\n" добавляет 2 строки вместо одной
+            else:
+                print(note)
             print(model.noteSep)
 
     def logOut(model):
         model.currentMenu = model.logInScreen
+        model.users[model.currentUser.name] = model.currentUser.notes
         model.currentUser = None
-
+                
     def exit(model):
         print("До свидания!")
         model.currentMenu = None
@@ -89,9 +117,12 @@ class Model:
     @property
     def userMenu(model):
         return Menu(model, [
-            MenuItem(
+            MenuItem(   # задание 1
                 text = "Добавить заметку",
                 command = model.addNote),
+            MenuItem(
+                text = "Удалить заметку",
+                command = model.delNote),
             MenuItem(
                 text = "Показать заметки",
                 command = model.listNotes),
